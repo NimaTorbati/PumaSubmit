bee4b772f4794482b3fb2b0de5cdc728d6a5861f
NimaTorbati <nima.torbati@gmail.com>
2025-03-21 13:44:00 +0100

    Final Track 1

---
+++ Docker/DockerTrack2/inference.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+
+# Step 1: perform tissue inference
+
+cd ./inference
+python inference_4th_submission.py
+#python inference_cll_tissue.py
+python inference_cell_tissue_unetpp.py
+python inference_tissue_from_nuclei.py
+#python inference_cell_tissue_unet.py
+#python inference_tissue_from_nuclei.py
+#python inference_cell_tissue_unet.py
+
+cd ../
+
+
+# Step 2: perform nuclei inference with hovernext
+python process.py "$@"
+
+
