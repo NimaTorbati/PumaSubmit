2c8f40b62ac1fda6d3ec4cd742955a225bfed9db
NimaTorbati <nima.torbati@gmail.com>
2025-07-29 17:33:10 +0200

    Final Track 1

--- Docker/DockerTrack2/inference.sh
+++ Docker/DockerTrack2/inference.sh
@@ -6,7 +6,7 @@
 cd ./inference
 python inference_4th_submission.py
 #python inference_cll_tissue.py
-python inference_cell_tissue_unetpp.py
+python inference_cell_tissue_unetpp10.py
 python inference_tissue_from_nuclei.py
 #python inference_cell_tissue_unet.py
 #python inference_tissue_from_nuclei.py
